5ae2263b452f35cb3d1e7c0c9c03ea2e0a7285bc
Lucas França <133548447+LuccasFranca@users.noreply.github.com>
2025-12-14 20:32:21 +0000

    Commit

--- xadrez.c
+++ xadrez.c
@@ -7,43 +7,78 @@
 int main() {
     // Nível Novato - Movimentação das Peças 
     printf("Desafio Jogo de Xadrez\n");
-    
-    // Implementação de Movimentação do Bispo
-    int casaBispo = 7;
-    int contadorBispo = 0;
-    printf("Movimento do Bispo\n");
 
-    while (contadorBispo < casaBispo)
+    //Menu interativo
+    int opcao;
+
+    printf("Escolha uma peca para mover\n");
+    printf("1. Bispo\n");
+    printf("2. Torre\n");
+    printf("3. Rainha\n");
+    printf("4. Cavalo\n");
+    scanf("%d", &opcao);
+    switch (opcao)
     {
-        printf("CimaEsquerda\n");
-        contadorBispo++;
-    }
-    printf("\n"); 
+    case 1://movimento do bispo
+        int casaBispo = 7;
+        int contadorBispo = 0;
+        printf("Movimento do Bispo\n");
 
-    // Implementação de Movimentação da Torre
-    int casaTorre = 5;
-    printf("Movimento da Torre:\n");
+        while (contadorBispo < casaBispo)
+        {
+            printf("CimaEsquerda\n");
+            contadorBispo++;
+        }
+        break;
+    case 2://movimento da torre
+        int casaTorre = 5;
+        printf("Movimento da Torre:\n");
 
-    for (int i = 1; i <= casaTorre; i++){
-        printf("Direita\n");
+        for (int i = 1; i <= casaTorre; i++)
+        {
+            printf("Direita\n");
+        }
+        break;
+    case 3://movimento da rainha
+        int casaRainha = 8;
+        int contadorRainha = 0;
+        printf("Movimento da Rainha\n");
+
+        do {
+            printf("Esquerda\n");
+            contadorRainha++;
+        } while (contadorRainha < casaRainha);
+        break;
+    case 4://movimento do cavalo
+        int casasBaixo = 2;
+        int casasEsquerda = 1;
+        int passo;
+        printf("Movimentação do Cavalo\n");
+        // Um loop pode representar a movimentação horizontal e outro vertical.
+        for (passo = 0; passo < casasBaixo; passo++){
+            int contador = 0;
+        
+            while (contador < 1)
+            {
+                printf("Baixo\n");
+                contador++;
+            }
+        }
+        int contadorEsq = 0;
+
+        while (contadorEsq < casasEsquerda){
+            printf("Esquerda\n");
+            contadorEsq++;
+        }
+        break;
+    default:
+        printf("opcao invalida!");
+        break;
     }
     printf("\n");
-
-    // Implementação de Movimentação da Rainha
-    int casaRainha = 4;
-    int contadorRainha = 0;
-    printf("Movimento da Rainha\n");
-
-    do {
-        printf("Esquerda\n");
-        contadorRainha++;
-    } while (contadorRainha < casaBispo);
     
-    printf("\n");
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    
-    // Um loop pode representar a movimentação horizontal e outro vertical.
 
     // Nível Mestre - Funções Recursivas e Loops Aninhados
     // Sugestão: Substitua as movimentações das peças por funções recursivas.
